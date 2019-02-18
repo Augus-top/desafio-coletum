760d556d57884cd85002da8d0d6ff80c839cc7e3
Augus-top <acpluschkat@hotmail.com>
2019-02-17 23:35:50 -0300

    Updates

---
+++ precache-manifest.05717a068065e5e6a952677d4e55c5fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99f0ecbf12703690a5ca",
+    "url": "/desafio-coletum/static/js/runtime~main.99f0ecbf.js"
+  },
+  {
+    "revision": "61c784ffff71717bb3ea",
+    "url": "/desafio-coletum/static/js/main.61c784ff.chunk.js"
+  },
+  {
+    "revision": "09542b4e347b96d9b915",
+    "url": "/desafio-coletum/static/js/2.09542b4e.chunk.js"
+  },
+  {
+    "revision": "09542b4e347b96d9b915",
+    "url": "/desafio-coletum/static/css/2.f451a16b.chunk.css"
+  },
+  {
+    "revision": "b8153cd7e40c8f00f31304b7e8f39a75",
+    "url": "/desafio-coletum/index.html"
+  }
+];
